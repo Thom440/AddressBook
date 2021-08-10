0199830fda125bf0aeb4c43a4ab75750632a1839
Thom Barrett <thom440@yahoo.com>
2021-08-10 14:08:50 -0700

    Address list box populates list on form load

--- AddressBook/Form1.cs
+++ AddressBook/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AddressBook.Class;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,12 @@ namespace AddressBook
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Class.AddressBook> addressBooks = AddressDB.GetAddressBooks();
+            addressListBox.DataSource = addressBooks.ToList();
         }
     }
 }
