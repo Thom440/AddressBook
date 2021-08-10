25cffcc97b3ca7a75c1e0470f9cc9855d9c8c0a8
Thom Barrett <thom440@yahoo.com>
2021-08-10 16:03:38 -0700

    Now able to open address book. Now able to delete address book

--- AddressBook/Form1.cs
+++ AddressBook/Form1.cs
@@ -36,5 +36,58 @@ namespace AddressBook
             List<Class.AddressBook> addressBooks = AddressDB.GetAddressBooks();
             addressListBox.DataSource = addressBooks;
         }
+
+        private void OpenAddressBook_Click(object sender, EventArgs e)
+        {
+            Class.AddressBook addressBook = (Class.AddressBook)addressListBox.SelectedItem;
+            if (addressBook == null)
+            {
+                MessageBox.Show("Must select an address book from the list");
+            }
+            else
+            {
+                openAddressBook.Enabled = false;
+                openAddressBook.Visible = false;
+
+                addAddressBook.Enabled = false;
+                addAddressBook.Visible = false;
+
+                deleteAddressBook.Enabled = false;
+                deleteAddressBook.Visible = false;
+
+                createContactBtn.Enabled = true;
+                createContactBtn.Visible = true;
+
+                openContactBtn.Enabled = true;
+                openContactBtn.Visible = true;
+
+                closeAddressBookBtn.Enabled = true;
+                closeAddressBookBtn.Visible = true;
+
+                deleteContactBtn.Enabled = true;
+                deleteContactBtn.Visible = true;
+
+                Class.AddressBook currentBook = AddressDB.GetAddressBook(addressBook.AddressBookName);
+                List<Person> people = currentBook.People;
+                addressListBox.DataSource = people;
+            }
+            
+        }
+
+        private void CreateContactBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeleteAddressBook_Click(object sender, EventArgs e)
+        {
+            Class.AddressBook addressBook = (Class.AddressBook)addressListBox.SelectedItem;
+            DialogResult result = MessageBox.Show($"Do you want to delete {addressBook.AddressBookName}", "Confirmation", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                AddressDB.Delete(addressBook.AddressBookName);
+            }
+            
+        }
     }
 }
