7c876605f1c050ec09cdd30fabcbcd97913ba083
Thom Barrett <thom440@yahoo.com>
2021-08-10 14:16:35 -0700

    Created Create Address Book Form

---
+++ AddressBook/CreateAddressBook.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace AddressBook
+{
+    partial class CreateAddressBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.addressBookNameTxtBox = new System.Windows.Forms.TextBox();
+            this.createBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name of Address Book";
+            // 
+            // addressBookNameTxtBox
+            // 
+            this.addressBookNameTxtBox.Location = new System.Drawing.Point(13, 30);
+            this.addressBookNameTxtBox.Name = "addressBookNameTxtBox";
+            this.addressBookNameTxtBox.Size = new System.Drawing.Size(220, 20);
+            this.addressBookNameTxtBox.TabIndex = 1;
+            // 
+            // createBtn
+            // 
+            this.createBtn.Location = new System.Drawing.Point(13, 57);
+            this.createBtn.Name = "createBtn";
+            this.createBtn.Size = new System.Drawing.Size(105, 23);
+            this.createBtn.TabIndex = 2;
+            this.createBtn.Text = "Create";
+            this.createBtn.UseVisualStyleBackColor = true;
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(125, 57);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(108, 23);
+            this.cancelBtn.TabIndex = 3;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // CreateAddressBook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 102);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.createBtn);
+            this.Controls.Add(this.addressBookNameTxtBox);
+            this.Controls.Add(this.label1);
+            this.Name = "CreateAddressBook";
+            this.Text = "Create Address Book";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox addressBookNameTxtBox;
+        private System.Windows.Forms.Button createBtn;
+        private System.Windows.Forms.Button cancelBtn;
+    }
+}
